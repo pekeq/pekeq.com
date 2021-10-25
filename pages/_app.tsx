3b235e4fb5f8201e4cb61229302954ae22afb66e
hideo-m <pekeq.9@gmail.com>
2021-10-25 09:16:18 +0900

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,47 @@
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
+import Script from 'next/script'
 import 'tailwindcss/tailwind.css'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <>
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
+    {/* Global Site Tag (gtag.js) - Google Analytics */}
+    <Script
+      strategy="afterInteractive"
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+    />
+    <Script
+    id="gtag-init"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtag.GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+    }}
+      />
     <Component {...pageProps} />
   </>
 }
